2c857a7c2178528d09bc3f6a4d862bcd25c9b4e9
0xFireball <0xFireball@outlook.com>
2016-10-11 22:25:51 -0700

    add project files

---
+++ NancyCore/NancyCoreBootstrapper.cs
@@ -0,0 +1,28 @@
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.TinyIoc;
+
+namespace NancyCore
+{
+    public class NancyCoreBootstrapper : DefaultNancyBootstrapper 
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("assets", "wwwroot/assets/")
+            );
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("static", "wwwroot/static/")
+            );
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            
+            //TODO: Do any required initialization for Nancy here
+        }
+    }
+}
